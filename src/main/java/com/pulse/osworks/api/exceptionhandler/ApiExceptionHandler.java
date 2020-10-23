d02ab755302f036a771805413841cd55b2c11580
Leonardo <leonardo.viana@armateus.com.br>
2020-10-23 12:27:34 -0300

    Feature(spring-rest-api):  Exception

---
+++ src/main/java/com/pulse/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,68 @@
+package com.pulse.osworks.api.exceptionhandler;
+
+
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.pulse.osworks.domain.exception.NegocioException;
+
+
+//componente do spring onde será colocado os tratamentos de exception de todos os controllers
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request) {
+		var status = HttpStatus.BAD_REQUEST;
+		var problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo(ex.getMessage());
+		problema.setDataHora(LocalDateTime.now());
+		
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, 
+			HttpHeaders headers, HttpStatus status, WebRequest request){
+		
+		var campos = new ArrayList<Problema.Campo>();
+		
+		for (ObjectError error : ex.getBindingResult().getAllErrors())  {
+			//String nome = error.getObjectName();
+			String nome = ((FieldError) error).getField();
+			// String mensagem = error.getDefaultMessage(); => Pega a mensagem padrão
+			//LocaleContextHolder => Pega a língua e região
+			String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+			campos.add(new Problema.Campo(nome, mensagem));
+		}
+		
+		
+		var problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo("Um ou mais campos estão inválidos."
+				+ " revise os dados e tente novamente");
+		problema.setDataHora(LocalDateTime.now());
+		problema.setCampos(campos);
+		
+		return super.handleExceptionInternal(ex, problema, headers, status, request);
+	}
+}
